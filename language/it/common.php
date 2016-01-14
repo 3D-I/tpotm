36a363b80b30a3533391704833c433e87d903897
3Di <three3di@hotmail.it>
2016-01-14 23:13:03 +0100

    langs EN and IT updated - v 1.0.3 RC4

--- language/it/common.php
+++ language/it/common.php
@@ -1,10 +1,10 @@
 <?php
 /**
 *
-* tpotm 1.0.2 [Italiano]
+* tpotm 1.0.3 [Italiano]
 *
-* @package phpBB Extension - tpotm 1.0.2-(Top Poster Of The Month)
-* @copyright (c) 2005 - 2008 - 2015 3Di (Marco T.)
+* @package phpBB Extension - tpotm 1.0.3-(Top Poster Of The Month)
+* @copyright (c) 2005 - 2008 - 2016 3Di (Marco T.)
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
 */
@@ -37,11 +37,15 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
 	'TPOTM_CAT'				=> 'Top Poster del Mese',
 	'TPOTM_NOW'				=> 'Attualmente è ',
-	'TPOTM_NOBODY'			=> '<strong>N/A</strong>',
-	'TPOTM_CACHE_EXPLAIN'	=> ' (<i>aggiornato ogni 15 minuti</i>)',
+	'TPOTM_NOBODY'			=> 'non disponibile, ',
+
+	'TPOTM_CACHE'	=> array(
+		1	=> ' (<i>aggiornato ogni<strong>%d</strong> minuto</i>)',
+		2	=> ' (<i>aggiornato ogni<strong>%d</strong> minuti</i>)',
+	),
 
 	'TPOTM_POST'	=> array(
-		1	=> ' con un totale di <strong>%d</strong> post',
-		2	=> ' con un totale di <strong>%d</strong> posts',
+		1	=> ' con un totale di<strong>%d</strong> post',
+		2	=> ' con un totale di<strong>%d</strong> posts',
 	),
 ));
