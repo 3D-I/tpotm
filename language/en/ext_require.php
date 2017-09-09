a8bd8c71b4aa2321a15153aa9f1db190d42a9645
3Di <three3di@hotmail.it>
2017-09-09 04:39:22 +0200

    Add new lang file

---
+++ language/en/ext_require.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * Top Poster Of The Month. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2005,2017, 3Di
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ERROR_MSG_3111_321_MISTMATCH'	=>	'Minimum 3.1.11 but less than 3.2.0@dev OR greater than 3.2.1',
+));
