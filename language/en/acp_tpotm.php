8c302aa6d71b930f021823d7dfe9218d060b65d5
3Di <three3di@hotmail.it>
2017-10-01 01:57:31 +0200

    Amend ACP wording

--- language/en/acp_tpotm.php
+++ language/en/acp_tpotm.php
@@ -24,7 +24,7 @@ $lang = array_merge($lang, array(
 	'ACP_TPOTM_TEMPLATE_LOCATIONS'			=>	'Template locations',
 	'ACP_TPOTM_TEMPLATE_LOCATIONS_EXPLAIN'	=>	'You may choose the location on index to be at the bottom or top of the page but both. In addition you might want to display the result also in viewforum.',
 	// Legend Hall of fame
-	'ACP_TPOTM_HALL'						=>	'HOF viewport settings',
+	'ACP_TPOTM_HALL'						=>	'Hall of fame settings',
 
 	// Tpl locations
 	'TPOTM_INDEX'							=>	'Index page',
@@ -32,20 +32,20 @@ $lang = array_merge($lang, array(
 	'TPOTM_TOP'								=>	'Top',
 	'TPOTM_FORUMS'							=>	'Forum pages',
 	// Hall of fame
-	'TPOTM_HALL'							=>	'Hall of fame',
-	'TPOTM_HALL_EXPLAIN'					=>	'Enable the page?',
+	'TPOTM_HALL'							=>	'Viewport',
+	'TPOTM_HALL_EXPLAIN'					=>	'Enable the page',
 	'TPOTM_USERS_PAGE'						=>	'Top posters',
-	'TPOTM_USERS_PAGE_EXPLAIN'				=>	'How many users to show at once',
+	'TPOTM_USERS_PAGE_EXPLAIN'				=>	'How many users to show per page',
 	// Variouses
-	'ACP_TPOTM_VARIOUSES'					=>	'Variouses',
+	'ACP_TPOTM_VARIOUSES'					=>	'Various settings',
 	'TPOTM_TTL'								=>	'Time to live',
-	'TPOTM_TTL_EXPLAIN'						=>	'Cache time in minutes. 0 = Disabled<br><em>Suggested to use the cache for better performances.</em>',
+	'TPOTM_TTL_EXPLAIN'						=>	'Cache time in minutes. 0 = Disabled<br><em>Use the cache to improve performance</em>',
 	'TPOTM_AVATAR'							=>	'Mini Avatar',
-	'TPOTM_AVATAR_EXPLAIN'					=>	'Prepends the TPOTM avatar to the result',
+	'TPOTM_AVATAR_EXPLAIN'					=>	'Display the user avatar in result',
 	'TPOTM_MINIPROFILE'						=>	'Mini profile next to posts',
-	'TPOTM_MINIPROFILE_EXPLAIN'				=>	'Display the TPOTM mini-badge',
+	'TPOTM_MINIPROFILE_EXPLAIN'				=>	'Display the mini-badge',
 	'TPOTM_ADM_MODS'						=>	'Admin and Moderators',
-	'TPOTM_ADM_MODS_EXPLAIN'				=>	'Include them in the TPOTM?<br><em>Excluding them increases DB queries.</em>',
+	'TPOTM_ADM_MODS_EXPLAIN'				=>	'Yes = improves performance<br><em>Excluding them increases DB load, the use of it is preferred in combination with the cache.</em>',
 	// Errors
 	'ACP_TPOTM_ERRORS'						=>	'Errors explaination',
 	'TPOTM_BADGE_IMG_INVALID'				=>	'The Badge filename is wrong or totally missing. Check your relative extension/style <strong>Images</strong> folder.<br>Badge filename must be: <strong>tpotm_badge.png</strong>.<br>Extension ACP it is now in a dormant status.<br>Fix the issue and it will automatically wake-up at runtime.',
