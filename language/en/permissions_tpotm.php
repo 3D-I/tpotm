8413d81c31f59daf388c01b6576dcac92475b551
3Di <three3di@hotmail.it>
2017-10-09 05:30:16 +0200

    Fix travis

--- language/en/permissions_tpotm.php
+++ language/en/permissions_tpotm.php
@@ -3,7 +3,7 @@
  *
  * Top Poster Of The Month. An extension for the phpBB Forum Software package.
  *
- * @copyright (c) 2005,2017, 3Di
+ * @copyright (c) 2005, 2017, 3Di
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
  */
@@ -15,7 +15,7 @@ if (!defined('IN_PHPBB'))
 
 if (empty($lang) || !is_array($lang))
 {
-	$lang = []);
+	$lang = [];
 }
 
 $lang = array_merge($lang, [
