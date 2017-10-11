70478fca74f7dcdbd8f21e9d7a15fbad86f50b33
3Di <three3di@hotmail.it>
2017-10-11 10:34:18 +0200

    SQL

--- core/tpotm.php
+++ core/tpotm.php
@@ -311,7 +311,8 @@ class tpotm
 	 */
 	public function wishes_founder()
 	{
-		return ($this->config['threedi_tpotm_founders']) ? '' : 'AND (u.user_type <> ' . USER_FOUNDER . ') ';
+		$tpotm_founder = (int) $this->config['threedi_tpotm_founders'];
+		return ($tpotm_founder) ? '' : 'AND (u.user_type <> ' . USER_FOUNDER . ') ';
 	}
 
 	/**
@@ -350,7 +351,8 @@ class tpotm
 	 */
 	public function wishes_admin_mods()
 	{
-		return ($this->config['threedi_tpotm_adm_mods']) ? '' : 'AND ' . $this->db->sql_in_set('u.user_id', $this->auth_admin_mody_ary(), true, true) . ' ';
+		$tpotm_admin_mods = (int) $this->config['threedi_tpotm_adm_mods'];
+		return ($tpotm_admin_mods) ? '' : 'AND ' . $this->db->sql_in_set('u.user_id', $this->auth_admin_mody_ary(), true, true) . ' ';
 	}
 
 	/**
@@ -391,7 +393,8 @@ class tpotm
 	 */
 	public function wishes_banneds()
 	{
-		return ($this->config['threedi_tpotm_banneds']) ? '' : 'AND ' . $this->db->sql_in_set('u.user_id', $this->banned_users_ids(), true, true) . ' ';
+		$tpotm_bans = (int) $this->config['threedi_tpotm_banneds'];
+		return ($tpotm_bans) ? '' : 'AND ' . $this->db->sql_in_set('u.user_id', $this->banned_users_ids(), true, true) . ' ';
 	}
 
 	/**
