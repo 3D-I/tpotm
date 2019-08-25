6c8e9882b5ac00df60034b61241d43062a54f175
3D-I <480857+3D-I@users.noreply.github.com>
2019-08-25 21:58:04 +0200

    v2.1.1 fix validation report

---
+++ migrations/m6_install_configs.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *
+ * Top Poster Of The Month. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2005, 2019, 3Di <https://www.phpbbstudio.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace threedi\tpotm\migrations;
+
+class m6_install_configs extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return phpbb_version_compare($this->config['threedi_tpotm'], '2.1.1', '>=');
+	}
+
+	static public function depends_on()
+	{
+		return ['\threedi\tpotm\migrations\m5_install_configs'];
+	}
+
+	public function update_data()
+	{
+		return [
+			['config.update', ['threedi_tpotm', '2.1.1']],
+		];
+	}
+}
